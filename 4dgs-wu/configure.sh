36df73810a4cd13ff5a7a8d5fad905b4f67e4d43
Lucas Yunkyu Lee <lucas@yklcs.com>
2024-04-27 16:44:01 +0000

    update 4dgs-wu

--- 4dgs-wu/configure.sh
+++ 4dgs-wu/configure.sh
@@ -5,6 +5,6 @@ set -euo pipefail
 export DEBIAN_FRONTEND=noninteractive
 
 . $VENV/bin/activate
-cd $WORKDIR/4dgs-wu
+cd 4dgs-wu
 pip install submodules/depth-diff-gaussian-rasterization
 pip install submodules/simple-knn
