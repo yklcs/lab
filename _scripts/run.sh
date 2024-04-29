383ef3250f5d5915e4bb783b0df690cabe2cdd26
Lucas Yunkyu Lee <lucas@yklcs.com>
2024-04-29 13:01:30 +0900

    disable jupyter token properly

--- _scripts/run.sh
+++ _scripts/run.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 
 source $VENV/bin/activate
 
-jupyter lab --ip=0.0.0.0 --no-browser --allow-root --NotebookApp.tokenUnicode="" &
+jupyter lab --ip=0.0.0.0 --no-browser --allow-root --NotebookApp.token="" &
 sleep infinity &
 
 wait
