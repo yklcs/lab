78e1ccb4b69d6dd877ce20778dd26a1859d0dd30
Lucas Yunkyu Lee <lucas@yklcs.com>
2024-04-29 12:04:44 +0900

    fix typos

--- _services/tailscale/run.sh
+++ _services/tailscale/run.sh
@@ -6,9 +6,8 @@ trap 'kill -TERM $PID' TERM INT
 echo "Starting Tailscale daemon"
 tailscaled --tun=userspace-networking --state=${TAILSCALE_STATE_ARG} ${TAILSCALE_OPT} &
 PID=$!
-until tailscale up --authkey="${TAILSCALE_AUTH+KEY}" --hostname="${TAILSCALE_HOSTNAME}"; do
+until tailscale up --authkey="${TAILSCALE_AUTH_KEY}" --hostname="${TAILSCALE_HOSTNAME}"; do
     sleep 0.1
 done
 tailscale status
-wait ${PID}
 wait ${PID}
